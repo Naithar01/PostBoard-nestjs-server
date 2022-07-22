b91845a738871de54eac459c7f60567f3d01d1ec
Naithar01 <eeung025@gmail.com>
2022-07-22 12:50:19 +0900

    Post Controller, Service, Module 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
+import { PostModule } from './post/post.module';
 
 @Module({
   imports: [
@@ -16,7 +17,10 @@ import { AppController } from './app.controller';
       entities: [],
       synchronize: true,
     }),
+    PostModule,
   ],
   controllers: [AppController],
 })
 export class AppModule {}
+
+// https://velog.io/@injoon2019/NestJS-%EC%9D%B8%ED%94%84%EB%9F%B0-%EB%94%B0%EB%9D%BC%ED%95%98%EB%A9%B0-%EB%B0%B0%EC%9A%B0%EB%8A%94-NestJS#nestjs-%EC%86%8C%EA%B0%9C
